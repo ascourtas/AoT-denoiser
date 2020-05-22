3c99ff29751060ccea175c2f1b7664f74231fc32
Aristana Scourtas <ascourtas@gmail.com>
2020-05-22 12:05:40 -0500

    amend stacy's script for personal use -- dirty

--- stacys_scripts/slurm_submit.sh
+++ stacys_scripts/slurm_submit.sh
@@ -19,6 +19,7 @@ START_DATE="2018-01-01"
 END_DATE="2019-06-30"
 ALL_AOT="/projects/b1045/AoT/AoT_Chicago.complete.2019-09-06/"
 SLICE_AOT="../data/AoT_Chicago.complete.recent.csv"
+MONTHLY_AOT_DIR="../data/chicago-complete.monthly.2019-04-01-to-2019-04-30"
 NEW_DIR="AoT_Chicago.complete.2019-09-06.from-$START_DATE-to-$END_DATE"
 
 echo "Starting AOT manipulation..."
@@ -28,33 +29,35 @@ echo "Starting AOT manipulation..."
 
 
 # make sure the path is an output of the previous script
-#gunzip /projects/b1045/AoT/$NEW_DIR/data.csv.gz
+gunzip ../data/$MONTHLY_AOT_DIR/data.csv.gz
+echo "1/7: Unzipped data file"
+#tar -xvf $MONTHLY_AOT
 #echo "1/7: Unzipped data file"
 
 #make a copy to save just in case, drop stuff from drop file
-#cp /projects/b1045/AoT/$NEW_DIR/data.csv /projects/b1045/AoT/$NEW_DIR/data_drop.csv
-#echo "2/7: Copy Done"
+cp ../data/$MONTHLY_AOT_DIR/data.csv ../data/$MONTHLY_AOT_DIR/data_backup.csv
+echo "2/7: Copy Done"
 
 # drop the variables that aren't specified – f1 = filein f2= fileout
 #python3 /home/asm0384/drop_unneeded_variables.py /projects/b1045/AoT/$NEW_DIR/data.csv /projects/b1045/AoT/$NEW_DIR/data_drop.csv
-python3 drop_unneeded_variables.py $SLICE_AOT ../data/data_drop.csv
+python3 drop_unneeded_variables.py ../data/$MONTHLY_AOT_DIR/data.csv ../data/$MONTHLY_AOT_DIR/data_drop.csv
 echo "3/7: Dropped unused variables"
 
 # copy dropped dataset with a switched header
 # TODO: ask Stacy why this was again -- I think it was something with the waggle tools
 #sed '1s/.*/timestamp,node_id,subsystem,sensor,parameter,value_hrf,value_raw/' /projects/b1045/AoT/$NEW_DIR/data_drop.csv > /projects/b1045/AoT/$NEW_DIR/data_head_diff.csv
-sed '1s/.*/timestamp,node_id,subsystem,sensor,parameter,value_hrf,value_raw/' ../data/data_drop.csv > ../data/data_head_diff.csv
+sed '1s/.*/timestamp,node_id,subsystem,sensor,parameter,value_hrf,value_raw/' ../data/$MONTHLY_AOT_DIR/data_drop.csv > ../data/$MONTHLY_AOT_DIR/data_head_diff.csv
 echo "4/7: Changed header"
 
 #save original dataset with original
-cp $SLICE_AOT ../data/data_original.csv
+cp ../data/$MONTHLY_AOT_DIR/data.csv ../data/$MONTHLY_AOT_DIR/data_original.csv
 echo "5/7: Saved original dataset"
 
 # make the new headed dataset the target for the next scripts 
-mv ../data/data_head_diff.csv ../data/data.csv
+mv ../data/$MONTHLY_AOT_DIR/data_head_diff.csv ../data/$MONTHLY_AOT_DIR/data.csv
 echo "6/7: Manipulating dataset"
 
 #Actually reduce the timestep of the 6 month dataset
-python3 ../../data-tools/data-reduction-tool/dataReduction.py -v 10000 -i ../data/ -t 30m
+python3 ../../data-tools/data-reduction-tool/dataReduction.py -v 10000 -i ../data/$MONTHLY_AOT_DIR/ -t 30m
 echo "7/7: Completed time averaging"
 echo "Completed AOT processing!"
