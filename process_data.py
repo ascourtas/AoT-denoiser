ae4d5ee01ec894daf12d5266ca03d7526dd33515
Aristana Scourtas <ascourtas@gmail.com>
2020-05-28 23:37:50 -0500

    add basic standard deviation anomaly-finding code

--- process_data.py
+++ process_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_all_CO_data(df):
@@ -10,15 +11,48 @@ def get_all_CO_data(df):
             co_df = co_df.append(row)
     return co_df
 
+def reformat_data(df):
+    # start with iterrows for simplicity, upgrade to apply() for performance
+    new_df = pd.DataFrame(columns=['timestamp', 'node_id'])
+    for index, row in df.iterrows():
+        # create new column if it doesn't already exist
+        # add data to that column
+        pass
+
+# resource: https://towardsdatascience.com/5-ways-to-detect-outliers-that-every-data-scientist-should-know-python-code-70a54335a623
+def sd_analysis(values):
+    # Set upper and lower limit to 3 standard deviation
+    data_std = values.std()
+    data_mean = values.mean()
+    anomaly_cut_off = data_std * 3
+
+    lower_limit = data_mean - anomaly_cut_off
+    upper_limit = data_mean + anomaly_cut_off
+    print(lower_limit)
+
+    # find outliers
+    out_series = values.loc[(values > upper_limit) | (values < lower_limit)]
+    return out_series
+
+# take in just a CO df
+def detect_anomalies(df):
+    sd_analysis(df["value_hrf"])
+
+
 
 def main():
     path_monthly = "data/chicago-complete.monthly.2019-04-01-to-2019-04-30/chicago-complete.monthly.2019-04-01-to-2019-04-30_reduced_data_30m/data.csv"
     path_weekly = "data/chicago-complete.weekly.2020-04-08-to-2020-04-14/weekly_April_08_to_14_data_drop.csv"
     df = pd.read_csv(path_weekly)
     print(df.head())
-    co_df = get_all_CO_data(df)
+
+    # co_df = get_all_CO_data(df)
+    path_weekly_co = "data/weekly_april_08_to_14_co_data.csv"
+    co_df = pd.read_csv(path_weekly_co)
+    print(co_df.head())
+    detect_anomalies(co_df)
     # co_df.to_csv("data/monthly_april_2019_reduced_co_data.csv")
-    co_df.to_csv("data/weekly_april_08_to_14_co_data.csv")
+    # co_df.to_csv("data/weekly_april_08_to_14_co_data.csv")
 
 
 
